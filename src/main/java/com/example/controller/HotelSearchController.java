76db835bd55c4b0c6809cebbf6ce28e0402ce1d7
tnakamura0314 <reggae.a0215@gmail.com>
2021-08-17 16:38:12 +0900

    HotelSearchController.javaの修正

--- src/main/java/com/example/controller/HotelSearchController.java
+++ src/main/java/com/example/controller/HotelSearchController.java
@@ -13,6 +13,12 @@ import com.example.domain.HotelSearch;
 import com.example.form.HotelSearchForm;
 import com.example.service.HotelSearchService;
 
+/**
+ * ホテルの情報を操作するコントローラー.
+ * 
+ * @author nakamuratomoya
+ *
+ */
 @Controller
 @RequestMapping("/hotelSearch")
 public class HotelSearchController {
@@ -30,6 +36,14 @@ public class HotelSearchController {
 		return "hotel-search";
 	}
 	
+	/**
+	 * 指定価格から該当のホテル情報を取得する.
+	 * 
+	 * @param price 価格
+	 * @param hotelSearchForm　フォームクラス
+	 * @param model　requestスコープ
+	 * @return　該当のホテル情報
+	 */
 	@RequestMapping("/searchByLessThanPrice")
 	public String searchByLessThanPrice(Integer price, HotelSearchForm hotelSearchForm, Model model){
 		
